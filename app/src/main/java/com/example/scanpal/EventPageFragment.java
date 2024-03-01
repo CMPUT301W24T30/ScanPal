dc8bbd98bffbe1501d024643a1bffd3f0bb9f4ea
Yifeng Chen <yifeng15@ualberta.ca>
2024-03-01 16:40:31 -0700

    Add actions from EventPageFragment to AddEditEventFragment

--- app/src/main/java/com/example/scanpal/EventPageFragment.java
+++ app/src/main/java/com/example/scanpal/EventPageFragment.java
@@ -6,8 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EventPageFragment extends Fragment {
+
+    FloatingActionButton addEventButton;
 
     public EventPageFragment() {
 
@@ -15,9 +21,20 @@ public class EventPageFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.events_page, container, false);
 
-        return null;
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        addEventButton = view.findViewById(R.id.button_add_event);
+
+        addEventButton.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(EventPageFragment.this);
+            navController.navigate(R.id.addEvent);
+        });
+
     }
 }
