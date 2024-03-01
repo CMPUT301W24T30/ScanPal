c06f72c189e7f1a41b9448a29f81bb8ba5d8bbf9
Yifeng Chen <yifeng15@ualberta.ca>
2024-03-01 16:41:30 -0700

    Given a QR code in Bitmap, upload the image to Storage. Then, upload to Firestore

--- app/src/main/java/com/example/scanpal/EventController.java
+++ app/src/main/java/com/example/scanpal/EventController.java
@@ -1,7 +1,18 @@
 package com.example.scanpal;
 
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +25,7 @@ import java.util.Map;
 public class EventController {
 
     private final FirebaseFirestore database; // instance of the database
+    private final FirebaseStorage storage;
 
     /**
      * Constructs an EventController with a reference to a Firestore database.
@@ -21,7 +33,9 @@ public class EventController {
      * @param database The Firestore database instance used for event operations.
      */
     public EventController(FirebaseFirestore database) {
+        // Why not just call FirebaseFireStore.getInstance()?
         this.database = database;
+        storage = FirebaseStorage.getInstance();
     }
 
     /**
@@ -34,7 +48,9 @@ public class EventController {
     }
 
     /**
-     * Adds a new event to the Firestore database.
+     * Adds a new event to the Firestore database. Uploads the event qr code image first then uploads to Firestore.
+     *
+     * TODO still need to upload event promotion qrcode image.
      *
      * @param event The event to be added to the database.
      */
@@ -55,9 +71,25 @@ public class EventController {
         }
         eventMap.put("participants", participantRefs);
 
-        // Save to database
-        database.collection("Events").document(event.getId()).set(eventMap)
-                .addOnSuccessListener(aVoid -> System.out.println("Event added successfully!"))
-                .addOnFailureListener(e -> System.out.println("Error adding event: " + e.getMessage()));
+        // upload qr code image
+        StorageReference storageRef = storage.getReference();
+        StorageReference qrCodesRef = storageRef.child("qr-codes/" + event.getId() + ".png");
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/png")
+                .build();
+        UploadTask uploadTask = qrCodesRef.putBytes(event.getEventQRCodeByteArray(), metadata);
+        uploadTask
+                .addOnFailureListener(exception -> Log.e("FirebaseStorage", "Failed to upload qr code: " + exception.getMessage()))
+                .addOnSuccessListener(taskSnapshot -> {
+                    // qr code is uploaded to the storage; get download url
+
+                    taskSnapshot.getStorage().getDownloadUrl().addOnSuccessListener(uri -> {
+                        eventMap.put("eventQRCodeUrl", uri.toString());
+                        // Save to database
+                        database.collection("Events").document(event.getId()).set(eventMap)
+                                .addOnSuccessListener(aVoid -> System.out.println("Event added successfully!"))
+                                .addOnFailureListener(e -> System.out.println("Error adding event: " + e.getMessage()));
+                    }).addOnFailureListener(exception -> Log.e("FirebaseStorage", "Failed to get download url for event qr code" + exception.getMessage()));
+                });
     }
 }
