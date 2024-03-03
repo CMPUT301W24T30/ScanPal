0b138d0f2b1c168e56d32cab55286fa958392866
Nandan-18 <nandanramesh123@gmail.com>
2024-03-02 21:41:26 -0700

    chore: revamp profile page + edit profile page

--- app/src/main/java/com/example/scanpal/EditProfileFragment.java
+++ app/src/main/java/com/example/scanpal/EditProfileFragment.java
@@ -1,4 +1,133 @@
 package com.example.scanpal;
 
-public class EditProfileFragment {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class EditProfileFragment extends Fragment {
+
+    private ImageView profileImageView;
+    private Button saveButton;
+    private FloatingActionButton uploadButton, deleteButton;
+    private TextInputEditText username, firstName, lastName;
+    private ImageController imageController;
+    private Uri imageUri;
+    private final ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    imageUri = result.getData().getData();
+                    profileImageView.setImageURI(imageUri);
+                    uploadImageToFirebase(imageUri);
+                }
+            });
+    private UserController userController;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.edit_profile_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        profileImageView = view.findViewById(R.id.imageView);
+        uploadButton = view.findViewById(R.id.upload_button);
+        deleteButton = view.findViewById(R.id.delete_button);
+        saveButton = view.findViewById(R.id.save_button);
+
+        username = (TextInputEditText) ((TextInputLayout) view.findViewById(R.id.username)).getEditText();
+        firstName = (TextInputEditText) ((TextInputLayout) view.findViewById(R.id.first_name)).getEditText();
+        lastName = (TextInputEditText) ((TextInputLayout) view.findViewById(R.id.last_name)).getEditText();
+
+        imageController = new ImageController();
+        userController = new UserController(FirebaseFirestore.getInstance(), getContext());
+
+        uploadButton.setOnClickListener(v -> openGallery());
+        deleteButton.setOnClickListener(v -> profileImageView.setImageDrawable(null));
+
+        saveButton.setOnClickListener(v -> saveUserDetails());
+
+        fetchUserDetails();
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        pickImageLauncher.launch(intent);
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        imageController.uploadImage(imageUri,
+                taskSnapshot -> Toast.makeText(getContext(), "Image Uploaded Successfully", Toast.LENGTH_SHORT).show(),
+                e -> Toast.makeText(getContext(), "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private void fetchUserDetails() {
+        String storedUsername = userController.fetchStoredUsername();
+        if (storedUsername != null) {
+            userController.getUser(storedUsername, new UserFetchCallback() {
+                @Override
+                public void onSuccess(User user) {
+                    username.setText(user.getUsername());
+                    firstName.setText(user.getFirstName());
+                    lastName.setText(user.getLastName());
+                    if (user.getPhoto() != null) {
+                        Glide.with(EditProfileFragment.this).load(user.getPhoto()).into(profileImageView);
+                    }
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Toast.makeText(getContext(), "Failed to fetch user details: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    private void saveUserDetails() {
+        User updatedUser = new User(Objects.requireNonNull(username.getText()).toString(), Objects.requireNonNull(firstName.getText()).toString(), Objects.requireNonNull(lastName.getText()).toString());
+        if (imageUri != null) {
+            updatedUser.setPhoto(imageUri.toString());
+        }
+        userController.addUser(updatedUser, new UserAddCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getContext(), "User details updated successfully", Toast.LENGTH_SHORT).show();
+                NavController navController = NavHostFragment.findNavController(EditProfileFragment.this);
+                navController.navigate(R.id.save_profile_edits);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Toast.makeText(getContext(), "Failed to update user details: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/scanpal/ProfileFragment.java
+++ app/src/main/java/com/example/scanpal/ProfileFragment.java
@@ -1,90 +1,116 @@
 package com.example.scanpal;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
-import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileFragment extends Fragment {
 
     private ImageView profileImageView;
-    private FloatingActionButton uploadButton, deleteButton;
-    private TextInputEditText username, firstName, lastName;
-    private ImageController imageController;
-    private Uri imageUri;
-
-    private final ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                    imageUri = result.getData().getData();
-                    profileImageView.setImageURI(imageUri);
-                    uploadImageToFirebase(imageUri);
-                }
-            });
+    private FloatingActionButton buttonScan, buttonGoBack, buttonChat, buttonProfile, buttonYourEvents, buttonHomepage,
+            buttonEditProfile;
+    private TextView addUsername, firstName, lastName, homepage;
+    private UserController userController;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.edit_profile_page, container, false);
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_profile_page, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        initializeViews(view);
+        setupButtonListeners();
+
+        userController = new UserController(FirebaseFirestore.getInstance(), getContext());
+
+        String username = userController.fetchStoredUsername();
+        if (username != null) {
+            fetchUserDetails(username);
+        } else {
+            Toast.makeText(getContext(), "No username found in internal storage", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initializeViews(View view) {
         profileImageView = view.findViewById(R.id.imageView);
-        uploadButton = view.findViewById(R.id.upload_button);
-        deleteButton = view.findViewById(R.id.delete_button);
-
-        username = (TextInputEditText) ((TextInputLayout) view.findViewById(R.id.username)).getEditText();
-        firstName = (TextInputEditText) ((TextInputLayout) view.findViewById(R.id.first_name)).getEditText();
-        lastName = (TextInputEditText) ((TextInputLayout) view.findViewById(R.id.last_name)).getEditText();
-
-        imageController = new ImageController();
-
-        uploadButton.setOnClickListener(v -> openGallery());
-
-        deleteButton.setOnClickListener(v -> profileImageView.setImageDrawable(null));
-
-        fetchImageFromFirebase();
+        buttonScan = view.findViewById(R.id.button_scan);
+        buttonGoBack = view.findViewById(R.id.button_go_back);
+        buttonChat = view.findViewById(R.id.button_chat);
+        buttonProfile = view.findViewById(R.id.button_profile);
+        buttonYourEvents = view.findViewById(R.id.button_your_events);
+        buttonHomepage = view.findViewById(R.id.button_homepage);
+        buttonEditProfile = view.findViewById(R.id.button_edit_profile);
+        addUsername = view.findViewById(R.id.addUsername);
+        firstName = view.findViewById(R.id.first_name);
+        lastName = view.findViewById(R.id.last_name);
+        homepage = view.findViewById(R.id.homepage);
     }
 
-    private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        pickImageLauncher.launch(intent);
+    private void setupButtonListeners() {
+        buttonScan.setOnClickListener(v -> {
+            // TODO
+        });
+
+        buttonGoBack.setOnClickListener(v -> {
+            // TODO
+        });
+
+        buttonChat.setOnClickListener(v -> {
+            // TODO
+        });
+
+        buttonProfile.setOnClickListener(v -> {
+            // TODO
+        });
+
+        buttonYourEvents.setOnClickListener(v -> {
+            // TODO
+        });
+
+        buttonHomepage.setOnClickListener(v -> {
+            // TODO
+        });
+
+        buttonEditProfile.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(ProfileFragment.this);
+            navController.navigate(R.id.edit_profile);
+        });
     }
 
-    private void uploadImageToFirebase(Uri imageUri) {
-        imageController.uploadImage(imageUri,
-                taskSnapshot -> Toast.makeText(getContext(), "Image Uploaded Successfully 🙏", Toast.LENGTH_SHORT)
-                        .show(),
-                e -> Toast.makeText(getContext(), "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show());
-    }
+    private void fetchUserDetails(String username) {
+        userController.getUser(username, new UserFetchCallback() {
+            @Override
+            public void onSuccess(User user) {
+                requireActivity().runOnUiThread(() -> {
+                    addUsername.setText(user.getUsername());
+                    firstName.setText(user.getFirstName());
+                    lastName.setText(user.getLastName());
+                    profileImageView.setImageURI(Uri.parse(user.getPhoto()));
+                });
+            }
 
-    private void fetchImageFromFirebase() {
-        // wip
-        String imageUrl = "https://firebasestorage.googleapis.com/v0/b/scanpal-15383.appspot.com/o/onphone.png?alt=media&token=fcfebed3-8b32-464d-a77d-7a37e5dc2ad1";
-        Glide.with(this)
-                .load(imageUrl)
-                .into(profileImageView);
+            @Override
+            public void onError(Exception e) {
+            }
+        });
     }
 }
