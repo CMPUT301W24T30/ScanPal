279425e0917a3ff773092e1c5015f968ac5a17f4
adviislam10 <sahiislam4@gmail.com>
2024-03-17 23:43:22 -0600

    share event controller and share function

--- app/src/main/java/com/example/scanpal/EventDetailsFragment.java
+++ app/src/main/java/com/example/scanpal/EventDetailsFragment.java
@@ -29,7 +29,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 /**
@@ -119,6 +118,7 @@ public class EventDetailsFragment extends Fragment {
         organizerImage = view.findViewById(R.id.organizer_image);
         FloatingActionButton scanQR = view.findViewById(R.id.scan_code);
         FloatingActionButton profileButton = view.findViewById(R.id.button_profile);
+        FloatingActionButton shareButton = view.findViewById(R.id.event_shareButton);
         viewSignedUpUsersBtn = view.findViewById(R.id.view_signed_up_users_button);
 
         // Setup user and attendee controllers
@@ -241,6 +241,12 @@ public class EventDetailsFragment extends Fragment {
             bundle.putString("eventID", eventID);
 
             navController.navigate(R.id.view_signed_up_users, bundle);
+        });
+
+        // Share button functionality
+        shareButton.setOnClickListener(v -> {
+            ShareEventController shareEventController = new ShareEventController(getContext());
+            shareEventController.shareQrCode(eventID);
         });
 
         return view;
