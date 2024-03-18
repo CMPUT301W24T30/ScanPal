fe07c5a4e3f6e27351156e2b14bab8db6c32a5a2
adviislam10 <sahiislam4@gmail.com>
2024-03-17 18:13:02 -0600

    request for event qr or check in qr functionality

--- app/src/main/java/com/example/scanpal/ShowQrFragment.java
+++ app/src/main/java/com/example/scanpal/ShowQrFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -31,19 +35,43 @@ public class ShowQrFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.show_qr, container, false);
+        assert getArguments() != null;
 
-        //assert getArguments() != null;
-        // String eventID = getArguments().getString("event_id");
-        String imageName = "c121e9c7-800f-4942-8b65-6a56a88106f8-check-in.png";
+        String eventID = getArguments().getString("event_id");
+        String request = getArguments().getString("request");
+
+        // Show either check in code or event code based on request
+        if (request == "check-in") {
+            showCheckIn(eventID);
+        } else if (request == "event") {
+            TextView textView = view.findViewById(R.id.show_qr_title);
+            textView.setText("Event Details");
+            showEvent(eventID);
+        }
+
+        // Back button
+        Button back = view.findViewById(R.id.button_go_back);
+        back.setOnClickListener( v-> {
+            NavController navController = NavHostFragment.findNavController(ShowQrFragment.this);
+            navController.navigate(R.id.show_qr_to_eventDetails);
+        });
+
+        return view;
+    }
+
+    public void showCheckIn(String eventID) {
+        String imageName = eventID + "-check-in.png";
         StorageReference imageRef = storage.getReference().child("qr-codes/" + imageName);
+        View view = getView();
+        assert view != null;
         ImageView imageView = view.findViewById(R.id.qr_code);
 
-        // Retrieve the image from Firebase Storage
-        imageRef.getBytes(1024 * 1024)
+        // Get image from Firebase Storage
+        imageRef.getBytes(1024 * 1024) // allow 1MB
                 .addOnSuccessListener(new OnSuccessListener<byte[]>() {
                     @Override
                     public void onSuccess(byte[] bytes) {
-                        // Load the retrieved image bytes into the ImageView using Glide
+                        // Load bytes into the ImageView using Glide
                         Glide.with(ShowQrFragment.this)
                                 .load(bytes)
                                 .into(imageView);
@@ -52,12 +80,37 @@ public class ShowQrFragment extends Fragment {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        // Handle failure to retrieve image
+                        // Handle failure
                         Toast.makeText(getContext(), "Failed to retrieve image", Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
 
-        return view;
+    public void showEvent(String eventID) {
+        String imageName = eventID + "-event.png";
+        StorageReference imageRef = storage.getReference().child("qr-codes/" + imageName);
+        View view = getView();
+        assert view != null;
+        ImageView imageView = view.findViewById(R.id.qr_code);
+
+        // Get image from Firebase Storage
+        imageRef.getBytes(1024 * 1024) // allow 1MB
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        // Load bytes into the ImageView using Glide
+                        Glide.with(ShowQrFragment.this)
+                                .load(bytes)
+                                .into(imageView);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle failure
+                        Toast.makeText(getContext(), "Failed to retrieve image", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 }
