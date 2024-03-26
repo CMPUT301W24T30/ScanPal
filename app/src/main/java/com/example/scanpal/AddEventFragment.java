e61b6dec0a30354b18f223756408eb172586dafa
Nandan-18 <nandanramesh123@gmail.com>
2024-03-26 01:25:57 -0600

    chore: upload image on save & add progress bar

--- app/src/main/java/com/example/scanpal/AddEventFragment.java
+++ app/src/main/java/com/example/scanpal/AddEventFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,19 +51,19 @@ public class AddEventFragment extends Fragment {
     Event newEvent;
     EventController eventController;
     UserController userController;
+    ProgressBar progressBar;
     User Organizer;
     private Uri imageUri;
     private ImageView profileImageView;
-    private ImageController imageController;
     private final ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
                     imageUri = result.getData().getData();
                     profileImageView.setImageURI(imageUri);
-                    uploadImageToFirebase(imageUri);
                 }
             });
+    private ImageController imageController;
     private PlacesClient placesClient;
     private String selectedLocationName;
 
@@ -100,6 +101,7 @@ public class AddEventFragment extends Fragment {
         this.eventNameForm = view.findViewById(R.id.add_edit_event_Name);
         this.eventDescriptionForm = view.findViewById(R.id.add_edit_event_description);
         this.profileImageView = view.findViewById(R.id.add_edit_event_ImageView);
+        this.progressBar = view.findViewById(R.id.progressBar);
 
         imageController = new ImageController();
         userController = new UserController(FirebaseFirestore.getInstance(), view.getContext());
@@ -132,6 +134,7 @@ public class AddEventFragment extends Fragment {
             } else if (Integer.parseInt(attendeesForm.getText().toString()) < 1) {
                 Toast.makeText(view.getContext(), "Please allow at least 1 Attendee", Toast.LENGTH_LONG).show();
             } else {
+                progressBar.setVisibility(View.VISIBLE);
                 newEvent.setName(eventNameForm.getText().toString());
                 newEvent.setLocation(selectedLocationName);
                 newEvent.setDescription(eventDescriptionForm.getText().toString());
@@ -140,7 +143,9 @@ public class AddEventFragment extends Fragment {
                 newEvent.setAnnouncementCount(0L);
 
                 eventController.addEvent(newEvent);
+                uploadImageToFirebase(imageUri);
 
+                progressBar.setVisibility(View.GONE);
                 NavController navController = NavHostFragment.findNavController(AddEventFragment.this);
                 navController.navigate(R.id.addEditEventComplete);
                 ((MainActivity) requireActivity()).setNavbarVisibility(true);
