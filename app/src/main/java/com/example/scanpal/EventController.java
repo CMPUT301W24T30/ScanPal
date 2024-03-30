314c7c8ea7a06ca88c933fdf61a6816acfc4385b
Jeff Weston <21jeffweston@gmail.com>
2024-03-30 02:09:36 -0600

    updated EventController to work with updated event

--- app/src/main/java/com/example/scanpal/EventController.java
+++ app/src/main/java/com/example/scanpal/EventController.java
@@ -68,6 +68,8 @@ public class EventController {
         eventMap.put("photo", event.getPosterURI());
         eventMap.put("capacity", event.getMaximumAttendees());
         eventMap.put("announcementCount", 0L);
+        eventMap.put("trackLocation", event.isTrackLocation());
+        eventMap.put("locationCoords", event.getLocationCoords());
 
         DocumentReference organizerRef = database.collection("Users").document(event.getOrganizer().getUsername());
         eventMap.put("organizer", organizerRef);
@@ -251,6 +253,7 @@ public class EventController {
 
                             event.setLocation(Objects.requireNonNull(eventDoc.get("location")).toString());
                             event.setMaximumAttendees((long) eventDoc.get("capacity"));
+                            event.setTrackLocation(eventDoc.getBoolean("trackLocation"));
 
                             Uri imageURI = Uri.parse(Objects.requireNonNull(eventDoc.get("photo")).toString());
                             event.setPosterURI(imageURI);
@@ -291,6 +294,7 @@ public class EventController {
             eventMap.put("location", event.getLocation());
             eventMap.put("capacity", event.getMaximumAttendees());
             eventMap.put("announcementCount", event.getAnnouncementCount());
+            eventMap.put("trackLocation", event.isTrackLocation());
 
             DocumentReference eventRef = database.collection("Events").document(event.getId());
             eventRef.update(eventMap).addOnSuccessListener(aVoid -> callback.onSuccess(true)).addOnFailureListener(callback::onError);
