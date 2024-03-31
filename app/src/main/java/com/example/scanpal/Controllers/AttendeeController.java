b58fc6370ff2599ba2467859333180d9c4c3ff85
Nandan-18 <nandanramesh123@gmail.com>
2024-03-30 20:50:17 -0600

    chore: minor bugfix

--- app/src/main/java/com/example/scanpal/Controllers/AttendeeController.java
+++ app/src/main/java/com/example/scanpal/Controllers/AttendeeController.java
@@ -92,22 +92,14 @@ public class AttendeeController {
 
                         DocumentReference userRef = documentSnapshot.getDocumentReference("user");
                         DocumentReference eventRef = documentSnapshot.getDocumentReference("eventID");
+
                         if (userRef != null && eventRef != null) {
                             userRef.get().addOnSuccessListener(userDoc -> {
-                                if (userDoc.exists()) {
-                                    User user = userDoc.toObject(User.class);
-                                    if (user != null) {
-                                        user.setUsername(userDoc.getId());
-                                        attendee.setUser(user);
-                                        attendee.setEventID(eventRef.getId());
-                                        callback.onSuccess(attendee);
-                                    } else {
-                                        callback.onError(new Exception("Failed to deserialize user"));
-                                    }
-                                } else {
-                                    callback.onError(new Exception("User document not found"));
-                                }
+                                User user = userDoc.toObject(User.class);
+                                attendee.setUser(user);
                             }).addOnFailureListener(callback::onError);
+                            attendee.setEventID(eventRef.getId());
+                            callback.onSuccess(attendee);
                         } else {
                             callback.onError(new Exception("User reference or Event reference not found in attendee document"));
                         }
