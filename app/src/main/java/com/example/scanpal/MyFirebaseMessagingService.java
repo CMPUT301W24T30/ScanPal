5b1c95e96bcce9df945484510abea614e8931012
Sundance636 <s.clement636@gmail.com>
2024-04-07 17:50:48 -0600

    Handle foreground notifications

--- app/src/main/java/com/example/scanpal/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/scanpal/MyFirebaseMessagingService.java
@@ -1,10 +1,19 @@
 package com.example.scanpal;
 
+import android.content.Context;
+import android.os.Handler;
 import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -30,8 +39,35 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         String messageBody = message.getNotification().getBody();
 
-        Toast.makeText( getApplicationContext(), "You've Received a new Notification: " + messageBody, Toast.LENGTH_SHORT).show();
+        //Toast.makeText( getApplicationContext(), "You've Received a new Notification: " + messageBody, Toast.LENGTH_SHORT).show();
+        //View customView = LayoutInflater.from(getBaseContext()).inflate(R.layout.notif_popup, null);
 
+        //Snackbar.make(getApplicationContext(), customView , messageBody,Snackbar.LENGTH_SHORT).show();
+
+        //Snackbar mySnackbar = Snackbar.make(customView, "You've Received a new Notification: " + messageBody, BaseTransientBottomBar.LENGTH_SHORT);
+
+        //mySnackbar.show();
+        showCustomPopup(getApplicationContext(), messageBody);
+
+    }
+
+    private void showCustomPopup(Context context, String message) {
+
+        new Handler(Looper.getMainLooper()).post(() -> {
+            View customView = LayoutInflater.from(context).inflate(R.layout.notif_popup, null);
+            TextView textView = customView.findViewById(R.id.popup_text);
+            textView.setText(message);
+
+
+            // Create and show the pop-up window
+            PopupWindow popupWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            popupWindow.showAtLocation(customView, android.view.Gravity.TOP | android.view.Gravity.CENTER_HORIZONTAL, 0, 100);
+            //popupWindow.showAsDropDown(customView);
+
+            // Dismiss the pop-up window after a certain duration
+            customView.postDelayed(popupWindow::dismiss, 3000);
+
+        });
     }
 
 }
